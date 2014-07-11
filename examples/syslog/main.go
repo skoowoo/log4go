ba2ed4693ebf1f103a2b468ceff9b42ca0b9f678
skoo87 <marckywu@gmail.com>
2014-07-11 22:17:11 +0800

    add syslog writer

---
+++ examples/syslog/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	log "log4go"
+)
+
+func SetLog() {
+	w := log.NewSyslogWriter()
+    w.SetNetwork("udp")
+    w.SetAddr("127.0.0.1:514")
+    w.SetTag("log4go")
+
+	log.Register(w)
+	log.SetLevel(log.DEBUG)
+}
+
+func main() {
+	SetLog()
+	defer log.Close()
+
+	var name = "skoo"
+	log.Debug("log4go by %s", name)
+	log.Info("log4go by %s", name)
+	log.Warn("log4go by %s", name)
+	log.Error("log4go by %s", name)
+	log.Fatal("log4go by %s", name)
+}
---
+++ syslog_writer.go
@@ -0,0 +1,65 @@
+package log4go
+
+import (
+	"errors"
+	"log/syslog"
+)
+
+type ShortRecord Record
+
+func (r *ShortRecord) String() string {
+	return "<" + r.code + "> " + r.info
+}
+
+type SyslogWriter struct {
+	network string
+	addr    string
+	tag     string
+	writer  *syslog.Writer
+}
+
+func NewSyslogWriter() *SyslogWriter {
+	return &SyslogWriter{}
+}
+
+func (w *SyslogWriter) SetNetwork(network string) {
+	w.network = network
+}
+
+func (w *SyslogWriter) SetAddr(addr string) {
+	w.addr = addr
+}
+
+func (w *SyslogWriter) SetTag(tag string) {
+	w.tag = tag
+}
+
+func (w *SyslogWriter) Init() (err error) {
+	w.writer, err = syslog.Dial(w.network, w.addr, syslog.LOG_DEBUG, w.tag)
+	return
+}
+
+func (w *SyslogWriter) Write(r *Record) (err error) {
+	s := ((*ShortRecord)(r)).String()
+
+	switch r.level {
+	case DEBUG:
+		err = w.writer.Debug(s)
+
+	case INFO:
+		err = w.writer.Info(s)
+
+	case WARNING:
+		err = w.writer.Warning(s)
+
+	case ERROR:
+		err = w.writer.Err(s)
+
+	case FATAL:
+		err = w.writer.Crit(s)
+
+	default:
+		err = errors.New("Invalid level")
+	}
+	return
+}
